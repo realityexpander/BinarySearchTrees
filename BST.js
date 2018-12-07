98fb65515d14d1ea7f4163da5837cbf727b2b96a
Chris Athanas <realityexpanderdev@gmail.com>
2018-12-07 15:03:43 -0600

    Update BST.js

--- BST.js
+++ BST.js
@@ -6,7 +6,7 @@
 
 // From memory, write code snippets for BFS and DFS of a BST.
 
-q1:
+// q1:
 
 function searchBT(arr, search) {
 
@@ -18,3 +18,27 @@ function searchBT(arr, search) {
   }
 }
 
+// q2:
+
+q2: 
+
+function breadthFirstSearch(root, search) {
+  let queue = []
+  queue.push(root[0])
+
+  while(queue.length !== 0) {
+     for(let i=0; i<queue.length; i++) {
+        let node = queue.shift();
+        if(node.value === value) {
+           return node;
+        }
+        if(node.left) {
+           queue.push(tree[node.left])
+        }
+        if(node.right) {
+           queue.push(tree[node.right])
+        }
+     }
+  }
+  return null 
+}
