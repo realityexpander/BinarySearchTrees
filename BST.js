ce4c5f68159c0416971645befb0cb1da5dc4d1cb
Chris Athanas <realityexpanderdev@gmail.com>
2018-12-07 15:17:10 -0600

    Update BST.js

--- BST.js
+++ BST.js
@@ -63,3 +63,25 @@ function depthFirstSearch(root, search) {
   }
   return null;
 }
+
+
+// A correct answer:
+// QUESTION 1
+
+// https://gist.github.com/tim-hr/3795c84a7d8b53110d6a91f6812bd5fe
+
+// QUESTION 2
+
+// First, traversal, since that’s the heart of the matter:
+
+// Depth First (recursive) - https://gist.github.com/tim-hr/f589b6fa4c1d5572cbe29f00b6b305cc
+
+// Depth First (iterative) - https://gist.github.com/tim-hr/02fa9d004420ddc2e6cfbc91f7159053
+
+// Breadth First - https://gist.github.com/tim-hr/90f9b649f63e58f6dab73be062ab32ad
+
+// Now, actual search:
+
+// DFS - https://gist.github.com/tim-hr/d1609306da2ed58f662f669f2aa574d1
+
+// BFS - https://gist.github.com/tim-hr/6bfd67c97f8fe9ecac1db1b9d8ce517a
